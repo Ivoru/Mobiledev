a5d47d5967b2c2e5aa17556b7b4560047ddc7fba
Ivoru <bandmaster95@gmail.com>
2019-12-08 01:44:11 +0800

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,22 @@
 import 'package:flutter/material.dart';
-import 'socicon_icons.dart';
-
-
-import './home.dart';
-import './reminder.dart';
-import './budget.dart';
-import './expense.dart';
-import './settings.dart';
+import 'package:project/auth.dart';
+import 'package:project/auth_provider.dart';
+import 'package:project/root_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override 
-    State<StatefulWidget> createState(){
-      return MyAppState();
-    }
-
-
-}
-
-class MyAppState extends State<MyApp>{ 
-  int _selectedPage = 0;
-  final _pageOptions = [
-    HomePage(),
-    Reminderpage(),
-    Budgetpage(),
-    Expensepage(),
-    Settingspage(),
-  ];
-  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Budget it',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(appBar: AppBar(title: Text('Budget It'),),
-      body: _pageOptions[_selectedPage],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red,
-        fixedColor: Colors.red,
-        currentIndex: _selectedPage,
-        onTap: (int index) {
-          setState(() {
-                        _selectedPage = index;
-
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("") 
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            title: Text("") 
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.attach_money),
-            title: Text("") 
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.money_off),
-            title: Text("") 
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text("") 
-          ),
-        ],
-      ),
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'Flutter login demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage(),
       ),
     );
   }
 }
-
